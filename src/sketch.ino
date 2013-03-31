fbfec587f38907149a5a8b770ac9d5fe07ff6303
mimmo <mimmo@Voltron.(none)>
2013-03-31 23:58:45 +0200

    second commit

--- src/sketch.ino
+++ src/sketch.ino
@@ -1,12 +1,54 @@
 
+#define CH_A 12 // Motore A Pin
+#define CH_B 13 // Motore A Pin
+
+#define BK_A 9
+#define BK_B 9
+
+#define SPIN_A 3
+#define SPIN_B 11
+
+#define LED 10
+
+#define FADE 5
+#define FADE_TIME 30
+
+int brightness=0;
+
 void setup()
 {
 	Serial.begin(9600);
+	pinMode(CH_A, OUTPUT);
+	pinMode(CH_B, OUTPUT);
+
+	pinMode(BK_A, OUTPUT);
+	pinMode(BK_A, OUTPUT);
+
+	pinMode(LED, OUTPUT);
+}
+
+void bright(int diff)
+{
+	while( brightness <= 255 && brightness <= brightness+diff)
+	{
+		analogWrite(LED, brightness);
+		brightness=brightness + FADETIME;
+	}
+	delay(FADE_TIME);
+}
+
+void fade(int diff)
+{
+	while( brightness <= 255 && brightness <= brightness+diff)
+	{
+		analogWrite(LED, brightness);
+		brightness=brightness + FADETIME;
+	}
+	delay(FADE_TIME);
 }
 
 void loop()
 {
-	char val='c';
 	int numBytes=0;
 	while (1)
 	{	
@@ -18,12 +60,7 @@ void loop()
 			{
 				buff[i]=Serial.read();
 			}
-		}
-	//	val = Serial.read();
-		delay(10);
-		if (val != -1)
-		{
-			Serial.println(val);
+			Serial.println(i);
 		}
 		delay(10);
 	}
